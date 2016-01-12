a7748fac5d3330015acd3ea35615512372fad0df
Christian Riboldi <criboldi@familysearch.com>
2016-01-12 00:30:34 -0700

    Wraps the navbar in a template.

--- js/home.js
+++ js/home.js
@@ -5,7 +5,6 @@ document.write("\
 	<link rel='stylesheet' type='text/css' href='../css/home.css'>\
 	<link href='http://fonts.googleapis.com/css?family=Walter+Turncoat' rel='stylesheet' type='text/css'>\
 	<link href='http://fonts.googleapis.com/css?family=Kreon' rel='stylesheet' type='text/css'>\
-  <script src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js'></script>\
   <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js' integrity='sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS' crossorigin='anonymous'></script>\
 \
-")
+");
---
+++ templates/navbar.js
@@ -0,0 +1,41 @@
+document.write("\
+<nav class='navbar navbar-default'>\
+	<div class='container-fluid'>\
+		<div class='navbar-header'>\
+			<button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1' aria-expanded='false'>\
+				<span class='icon-bar'></span>\
+				<span class='icon-bar'></span>\
+				<span class='icon-bar'></span>\
+			</button>\
+			<a class='navbar-brand logo' href='#'>\
+				<img src='../img/logo50x50.png'>\
+			</a>\
+		</div>\
+		<div class='collapse navbar-collapse'>\
+			<ul class='nav navbar-nav'>\
+				<li class='dropdown'>\
+					<a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>About Me <span class='caret'></span>\
+					</a>\
+					<ul class='dropdown-menu'>\
+						<li><a href='#'>Family</a></li>\
+						<li><a href='#'>Church</a></li>\
+						<li><a href='#'>School</a></li>\
+						<li><a href='#'>Hobbies</a></li>\
+					</ul>\
+				</li>\
+				<li><a href='#'>Resume</a></li>\
+				<li class='dropdown'>\
+					<a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>Weather <span class='caret'></span>\
+					</a>\
+					<ul class='dropdown-menu'>\
+						<li><a href='#'>Current</a></li>\
+						<li><a href='#'>Forcast</a></li>\
+					</ul>\
+				</li>\
+				<li><a href='#'>Snake</a></li>\
+			</ul>\
+		</div>\
+	</div>\
+</nav>\
+\
+");
