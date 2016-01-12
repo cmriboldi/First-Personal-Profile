ac32a3d27a0aca51e054ef7ab1828a7df5a803b6
Christian Riboldi <criboldi@familysearch.com>
2016-01-11 21:38:36 -0700

    Adds splash page and basic home page

---
+++ js/home.js
@@ -0,0 +1,11 @@
+document.write("\
+\
+	<link rel='icon' type='img/x-icon' href='../images/AAflags.ico'>\
+  <link rel='stylesheet' href='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'>\
+	<link rel='stylesheet' type='text/css' href='../css/home.css'>\
+	<link href='http://fonts.googleapis.com/css?family=Walter+Turncoat' rel='stylesheet' type='text/css'>\
+	<link href='http://fonts.googleapis.com/css?family=Kreon' rel='stylesheet' type='text/css'>\
+  <script src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js'></script>\
+  <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js' integrity='sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS' crossorigin='anonymous'></script>\
+\
+")
---
+++ js/import.js
@@ -0,0 +1,9 @@
+document.write("\
+\
+	<link rel='icon' type='image/x-icon' href='/images/AAflags.ico'>\
+  <link rel='stylesheet' href='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'>\
+	<link rel='stylesheet' type='text/css' href='css/style.css'>\
+	<link href='http://fonts.googleapis.com/css?family=Walter+Turncoat' rel='stylesheet' type='text/css'>\
+	<link href='http://fonts.googleapis.com/css?family=Kreon' rel='stylesheet' type='text/css'>\
+\
+")
